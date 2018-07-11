7333eaaa26750aa7f2fc2ac23503d22dd47f23f5
kosatun@aol.com <kosatun@aol.com>
2018-07-11 22:05:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,15 +2,14 @@
 def valid_move?(board, index)
   
   
+  
   if position_taken?(board, index) == false 
     return true
     
   elsif position_taken?(board, index) == true
     return false
     
-   elsif index.between?(0, 8) 
-      
-      return nil
+   els
   
   end   
   
